49560fbb65e582ffdeb4248b0b9223d83512cd57
alexlee27 <alexlee7942@gmail.com>
2023-09-11 16:22:26 -0400

    more stuff

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 // my comment
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
